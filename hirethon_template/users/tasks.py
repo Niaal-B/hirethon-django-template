6a3a9c47ee872f30f3fd1682b3d846088ae42e88
Niaal-B <nihnihal504@gmail.com>
2025-10-18 20:17:28 +0530

    feat(users): added a celery task to send mail

--- hirethon_template/users/tasks.py
+++ hirethon_template/users/tasks.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from celery import shared_task
 from config import celery_app
+from django.core.mail import send_mail
 
 User = get_user_model()
 
@@ -9,3 +10,17 @@ User = get_user_model()
 def get_users_count():
     """A pointless Celery task to demonstrate usage."""
     return User.objects.count()
+
+
+
+@shared_task
+def send_verification_email_task(email, verify_url):
+    subject = "Verify your email"
+    message = f"Click the link to verify your account: {verify_url}"
+    send_mail(
+        subject=subject,
+        message=message,
+        from_email="noreply@hirethon.local",
+        recipient_list=[email],
+        fail_silently=False,
+    )
